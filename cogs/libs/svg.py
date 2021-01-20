efdcd8093042c154ffe514459bebaf9840f17cbe
billyeatcookies <billydevbusiness@gmail.com>
2021-01-20 11:38:33 +0530

    ✨ SVG rendering

---
+++ cogs/libs/svg.py
@@ -0,0 +1,14 @@
+from chess import svg
+
+from cairosvg import svg2png
+
+
+def get_image(_svg_code):
+    svg2png(bytestring=_svg_code, write_to='output.png')
+
+
+def generate_image(_board, _squares=None):
+    if _squares:
+        get_image(svg.board(_board, squares=_squares, size=350))
+    else:
+        get_image(svg.board(_board, size=350))
