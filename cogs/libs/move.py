74ee27189cdb8744347c9efa1dad2b6a31d9154e
billyeatcookies <billydevbusiness@gmail.com>
2021-01-20 12:26:07 +0530

    🐛 fixed bug. - board wasn't updating after undo(fixed)

--- cogs/libs/move.py
+++ cogs/libs/move.py
@@ -1,15 +1,15 @@
-import chess
-
-from cogs.libs.formatter import format_content
-
 from cogs.libs.boards import boards
+
+from cogs.libs.svg import generate_image
 
 
 def push_san(_user_id, _move):
     _board = boards[_user_id]
-    return format_content(_board.push_san(_move))
+    _board.push_san(_move)
+    generate_image(_board)
 
 
 def undo(_user_id):
     _board = boards[_user_id]
-    return _board.pop()
+    _board.pop()
+    generate_image(_board)
